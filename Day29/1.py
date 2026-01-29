6a9133691268d5d5d22c6077ac18f72351078df7
shivamshekhar711-ops <shivamshekhar711@gmail.com>
2026-01-29 22:16:55 +0530

    New

---
+++ Day29/1.py
@@ -0,0 +1,42 @@
+# def add():
+#     a = 10
+#     b = 20
+#     print("Sum =", a + b)
+
+# add()
+
+
+
+
+
+
+# def greatest():
+#     a = 15
+#     b = 10
+#     if a > b:
+#         print("Greatest =", a)
+#     else:
+#         print("Greatest =", b)
+
+# greatest()
+
+
+
+
+
+
+# def add():
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     print("Sum =", a + b)
+
+# add()
+
+
+
+
+# def square():
+#     n = int(input("Enter a number: "))
+#     print("Square =", n * n)
+
+# square()
